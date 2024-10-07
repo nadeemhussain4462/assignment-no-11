a146bdf70d15252ad4b286eee589521eeb18ab69
Nadeem Hussain <hoteleyenadeem@gmail.com>
2024-10-07 09:41:12 +0500

    Assignment no 11

---
+++ app.js
@@ -0,0 +1,40 @@
+// Assignment no 11
+// chapter no 26 to 30  MATH METHODS
+// Question no 1 
+// Write a program that takes a positive integer from user & display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number
+
+
+
+
+// // Question no 2
+// var diceValue = Math.floor(Math.random() * 6) + 1;
+// document.write("Random dice value: " + diceValue + "<br>");
+// // Question no 3
+// var randomNumber = Math.floor(Math.random() * 2) + 1;
+// document.write(randomNumber + "<br>");
+// if (randomNumber === 1) {
+//     document.write("Random coin value: Tails<br>");
+// } else {
+//     document.write("Random coin value: Heads<br>");
+// }
+// // Question no 4
+// var randomNumber = Math.floor(Math.random() * 100) + 1;
+// document.write("Random number between 1 and 100: " + randomNumber + "<br>");
+// // Question no 5
+// var userWeight = prompt("Enter your weight in kilograms:");
+// var parsedWeight = parseFloat(userWeight);
+// document.write("Your weight is " + parsedWeight + " kilograms<br>");
+// // Question no 6
+// var secretNumber = Math.floor(Math.random() * 10) + 1;
+// var userGuess = prompt("Guess a number between 1 and 10:");
+// var parsedGuess = parseInt(userGuess);
+// if (parsedGuess === secretNumber) {
+//     document.write("Congratulations! Your guess is correct.<br>");
+// } else {
+//     document.write("Sorry, your guess is incorrect. The secret number was " + secretNumber + "<br>");
+// }
+
